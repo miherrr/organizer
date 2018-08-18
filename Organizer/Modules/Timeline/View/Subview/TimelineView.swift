565c79658e32620047be2c1339178b246ce5625c
Mikhail Babushkin <babushkin94@gmail.com>
2018-08-18 19:23:30 +0300

    fix background line add scrollview inset

--- Organizer/Modules/Timeline/View/Subview/TimelineView.swift
+++ Organizer/Modules/Timeline/View/Subview/TimelineView.swift
@@ -36,25 +36,7 @@ class TimelineView: UIView {
     
     // MARK: - Private vars
     private let eventsSpacing: CGFloat = 15
-    private let colors: [UIColor] = [
-        UIColor.colorFromRGB(rgbValue: 0x00c90d),
-        UIColor.colorFromRGB(rgbValue: 0x2219b2),
-        UIColor.colorFromRGB(rgbValue: 0xffc300),
-        UIColor.colorFromRGB(rgbValue: 0xff0700),
-        UIColor.colorFromRGB(rgbValue: 0x67e46f),
-        UIColor.colorFromRGB(rgbValue: 0x7872d8),
-        UIColor.colorFromRGB(rgbValue: 0xffde73),
-        UIColor.colorFromRGB(rgbValue: 0xff7673)
-    ]
 
-    private var currentColorIndex: Int = -1
-    private var nextColor: UIColor {
-        currentColorIndex += 1
-        if currentColorIndex >= colors.count {
-            currentColorIndex = 0
-        }
-        return colors[currentColorIndex]
-    }
     private var lineWidthConstraint: NSLayoutConstraint!
     private var container: UIView!
     private var eventsStackView: UIStackView!
@@ -82,13 +64,6 @@ class TimelineView: UIView {
         lineWidthConstraint = container.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.size.width)
         lineWidthConstraint.isActive = true
         
-        eventsStackView = UIStackView(frame: .zero)
-        container.addSubview(eventsStackView.prepareForAutoLayout())
-        eventsStackView.pinEdgesToSuperviewEdges()
-        eventsStackView.axis = .horizontal
-        eventsStackView.distribution = .equalSpacing
-        eventsStackView.spacing = eventsSpacing
-        
         let grayline = UIView()
         container.addSubview(grayline.prepareForAutoLayout())
         grayline.leadingAnchor ~= leadingAnchor
@@ -97,24 +72,29 @@ class TimelineView: UIView {
         grayline.heightAnchor ~= TimelineView.lineHeight
         grayline.layer.cornerRadius = TimelineView.lineHeight / 2
         grayline.backgroundColor = .lightGray
+        
+        eventsStackView = UIStackView(frame: .zero)
+        container.addSubview(eventsStackView.prepareForAutoLayout())
+        eventsStackView.pinEdgesToSuperviewEdges()
+        eventsStackView.axis = .horizontal
+        eventsStackView.distribution = .equalSpacing
+        eventsStackView.spacing = eventsSpacing
     }
     
     private func addEventToTimeline(model: TimelineModel) {
-        let eventView = TimelineEventView(data: model, color: nextColor)
+        let eventView = TimelineEventView(data: model)
         eventsStackView.addArrangedSubview(eventView.prepareForAutoLayout())
     }
     
     @IBDesignable
     class TimelineEventView: UIView {
         private let data: TimelineModel
-        private let fillColor: UIColor
         private var durationInHours: Double {
             return data.end.timeIntervalSince(data.start) / 3600.0
         }
         
-        init(data: TimelineModel, color: UIColor) {
+        init(data: TimelineModel) {
             self.data = data
-            self.fillColor = color
             super.init(frame: .zero)
             addSubviews()
         }
@@ -126,7 +106,9 @@ class TimelineView: UIView {
         private func addSubviews() {
             let label = UILabel()
             addSubview(label.prepareForAutoLayout())
-            label.topAnchor ~= topAnchor
+            let labelToTopConstraint = label.topAnchor.constraint(equalTo: topAnchor)
+//            labelToTopConstraint.priority = UILayoutPriority(rawValue: 749)
+            labelToTopConstraint.isActive = true
             label.leadingAnchor ~= leadingAnchor + 8
             label.trailingAnchor ~= trailingAnchor - 8
             label.textAlignment = .center
@@ -135,6 +117,7 @@ class TimelineView: UIView {
             
             let line = UIView()
             addSubview(line.prepareForAutoLayout())
+            line.bottomAnchor ~= bottomAnchor
             line.heightAnchor ~= TimelineView.lineHeight
             line.widthAnchor.constraint(lessThanOrEqualToConstant: TimelineView.maxEventWidth).isActive = true
             line.leadingAnchor ~= leadingAnchor
@@ -142,7 +125,7 @@ class TimelineView: UIView {
             let desiredWidthConstraint = line.widthAnchor.constraint(equalToConstant: CGFloat(durationInHours) * TimelineView.pointPerHout)
             desiredWidthConstraint.priority = UILayoutPriority(rawValue: 749)
             desiredWidthConstraint.isActive = true
-            line.backgroundColor = fillColor
+            line.backgroundColor = data.type.color
             line.layer.cornerRadius = TimelineView.lineHeight / 2
             
             line.topAnchor ~= label.bottomAnchor + 8
--- Organizer/Modules/Timeline/View/TimelineViewController.swift
+++ Organizer/Modules/Timeline/View/TimelineViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 
 class TimelineViewController: UIViewController {
 
+    @IBOutlet private weak var timelineScrollView: UIScrollView!
     @IBOutlet private  weak var timelineView: TimelineView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        timelineScrollView.contentInset = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8)
+        
         timelineView.data = [
             TimelineModel(id: 0, title: "Первое событие", start: Date(), end: Date().addingTimeInterval(10_000), type: EventType.important),
             TimelineModel(id: 1, title: "Второе событие", start: Date(), end: Date().addingTimeInterval(20_000), type: EventType.notImportant),
